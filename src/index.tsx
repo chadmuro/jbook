2d737881d178584d4c808808c1fea36ca1aedb80
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-02-15 23:33:09 +0900

    create monaco editor and add css

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
 	const ref = useRef<any>();
+	const iframe = useRef<any>();
 	const [input, setInput] = useState('');
-	const [code, setCode] = useState('');
 
 	const startService = async () => {
 		ref.current = await esbuild.startService({
@@ -25,6 +27,8 @@ const App = () => {
 			return;
 		}
 
+		iframe.current.srcdoc = html;
+
 		const result = await ref.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
@@ -36,13 +40,36 @@ const App = () => {
 			},
 		});
 
-		// console.log(result);
-
-		setCode(result.outputFiles[0].text);
+		// setCode(result.outputFiles[0].text);
+		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
 	};
+
+	const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
 	return (
 		<div>
+			<CodeEditor
+				initialValue="const a = 1;"
+				onChange={value => setInput(value)}
+			/>
 			<textarea
 				value={input}
 				onChange={e => setInput(e.target.value)}
@@ -50,7 +77,12 @@ const App = () => {
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
-			<pre>{code}</pre>
+			<iframe
+				title="preview"
+				ref={iframe}
+				sandbox="allow-scripts"
+				srcDoc={html}
+			/>
 		</div>
 	);
 };
