c3a59216a655ca546bd80f5ba8c72bb5de37d4a8
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-02-19 10:42:07 +0900

    add react-resizable to code-cell

---
+++ src/components/resizable.tsx
@@ -0,0 +1,37 @@
+import './resizable.css';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+
+interface ResizableProps {
+	direction: 'horizontal' | 'vertical';
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+			height: Infinity,
+			width: window.innerWidth * 0.75,
+			resizeHandles: ['e'],
+			maxConstraints: [window.innerWidth * 0.75, Infinity],
+			minConstraints: [window.innerWidth * 0.2, Infinity],
+		};
+  } else {
+    resizableProps = {
+      height: 300,
+			width: Infinity,
+			resizeHandles: ['s'],
+			maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24]
+    };
+  }
+
+	return (
+		<ResizableBox {...resizableProps}>
+			{children}
+		</ResizableBox>
+	);
+};
+
+export default Resizable;
