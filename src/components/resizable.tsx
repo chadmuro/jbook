5059ce96a7b65b89d7e7f523a9dc7c4c7d06e2d5
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-02-20 11:17:01 +0900

    error handling for async and bundling

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useState, useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -6,32 +7,55 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  let resizableProps: ResizableBoxProps;
+	let resizableProps: ResizableBoxProps;
+	const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+	const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
 
-  if (direction === 'horizontal') {
-    resizableProps = {
-      className: 'resize-horizontal',
-			height: Infinity,
-			width: window.innerWidth * 0.75,
-			resizeHandles: ['e'],
-			maxConstraints: [window.innerWidth * 0.75, Infinity],
-			minConstraints: [window.innerWidth * 0.2, Infinity],
+	useEffect(() => {
+		let timer: any;
+		const listener = () => {
+			if (timer) {
+				clearTimeout(timer);
+			}
+			timer = setTimeout(() => {
+				setInnerHeight(window.innerHeight);
+				setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+			}, 100);
 		};
-  } else {
-    resizableProps = {
-      height: 300,
+		window.addEventListener('resize', listener);
+
+		return () => {
+			window.removeEventListener('resize', listener);
+		};
+	}, [width]);
+
+	if (direction === 'horizontal') {
+		resizableProps = {
+			className: 'resize-horizontal',
+			height: Infinity,
+			width,
+			resizeHandles: ['e'],
+			maxConstraints: [innerWidth * 0.75, Infinity],
+			minConstraints: [innerWidth * 0.2, Infinity],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      }
+		};
+	} else {
+		resizableProps = {
+			height: 300,
 			width: Infinity,
 			resizeHandles: ['s'],
-			maxConstraints: [Infinity, window.innerHeight * 0.9],
-      minConstraints: [Infinity, 24]
-    };
-  }
+			maxConstraints: [Infinity, innerHeight * 0.9],
+			minConstraints: [Infinity, 24],
+		};
+	}
 
-	return (
-		<ResizableBox {...resizableProps}>
-			{children}
-		</ResizableBox>
-	);
+	return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
